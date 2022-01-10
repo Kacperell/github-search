1ee850fd33223ae037c1319472c9ff4dbdb1ee5f
Kacperell <kacperloczek3@gmail.com>
2022-01-11 00:42:01 +0100

    Add github token to .env file

--- src/config.tsx
+++ src/config.tsx
@@ -1,7 +1,2 @@
 export const githubSearchApiEndpoint = `https://api.github.com/graphql`;
 
-//Temporarily here. (Access to public data only)
-export const githubToken = `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`;
-
-
-
--- src/handlers/authConfig.tsx
+++ src/handlers/authConfig.tsx
@@ -1,9 +1,11 @@
-import { githubToken } from "../config";
+
+const GITHUB_API = process.env.REACT_APP_API_KEY;
 const authConfig = () => {
+    console.log(process.env);
     const config = {
         headers: {
             'Content-type': 'application/json',
-            authorization: `Bearer ${githubToken}`,
+            authorization: `Bearer ${GITHUB_API}`,
 
         }
     };
